f45c8d7f1871490ccf3d4808130625d9d42f1984
Maky188 <maky188pgt555@gmail.com>
2023-11-28 18:17:31 -0100

    Programa 3

--- Programa_3.py
+++ Programa_3.py
@@ -1,6 +1,3 @@
 def junta_ordenados(tuplo1, tuplo2):
-    for elemento in tuplo2:
-        tuplo1 = tuplo1 + (elemento, )
-    return sorted(tuplo1)  
-
+    return sorted (tuplo1 + tuplo2)  
 print(junta_ordenados((2, 34, 200, 210), (1, 23)))
